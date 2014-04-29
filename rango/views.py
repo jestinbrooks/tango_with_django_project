59899cf1f5d7af3ee83fde355dfceecadb8f4c32
Jestin Brooks <jestin7@gmail.com>
2014-04-29 12:53:32 -0700

    added

--- rango/views.py
+++ rango/views.py
@@ -2,14 +2,32 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
 
-from rango.models import Category
+from rango.models import Category, Page
 
 
 def index(request):
     context = RequestContext(request)
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
+    for category in category_list:
+        category.url = category.name.replace(' ', '_')
     return render_to_response('rango/index.html', context_dict, context)
+
+
+def category(request, category_name_url):
+    context = RequestContext(request)
+
+    #replace spaces in category names with _ to make urls more readable
+    category_name = category_name_url.replace('_', ' ')
+    context_dict = {'category_name': category_name}
+    try:
+        category = Category.objects.get(name=category_name)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        pass
+    return render_to_response('rango/category.html', context_dict, context)
 
 
 def about(request):
