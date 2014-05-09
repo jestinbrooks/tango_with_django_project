507f615b3fa606086d1433445d0be172bbd27784
Jestin Brooks <jestin7@gmail.com>
2014-05-09 10:17:06 -0700

    added delicious cookies

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
@@ -11,11 +13,28 @@ from rango.forms import UserProfileForm, UserForm
 
 def index(request):
     context = RequestContext(request)
+
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
+
     for category in category_list:
         category.url = category.name.replace(' ', '_')
-    return render_to_response('rango/index.html', context_dict, context)
+
+    response = render_to_response('rango/index.html', context_dict, context)
+
+    visits = int(request.COOKIES.get('visits', '0'))
+
+    if 'last_visit' in request.COOKIES:
+        last_visit = request.COOKIES['last_visit']
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+        if (datetime.now() - last_visit_time).days > 0:
+            response.set_cookie('visits', visits+1)
+            response.set_cookie('last_visit', datetime.now())
+    else:
+        response.set_cookie('last_visit', datetime.now())
+
+    return response
 
 
 def category(request, category_name_url):
